678976c446c438d68e19eef076c935b0505e25f4
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-08-21 11:20:30 +0200

    Movement left update

---
+++ Assets/Scripts/SquareController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 centerPosition = new Vector3(0, 0, 0);
+
+        // Transform sqareTransform = GetComponent<Transform>();
+
+        // sqareTransform.position = centerPosition;
+
+        gameObject.transform.position = centerPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = gameObject.transform.position;
+
+        position.x += 0.1f;
+
+        gameObject.transform.position = position;
+
+    }
+}
