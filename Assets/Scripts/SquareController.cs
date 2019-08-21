c82b4d1670ae3975e9236eb160129630529caa6a
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-08-21 11:44:26 +0200

    Updated left right movement & movement speed public

--- Assets/Scripts/SquareController.cs
+++ Assets/Scripts/SquareController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SquareController : MonoBehaviour
 {
+
+    public float speed = 0.1f;
+    float direction = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +24,29 @@ public class SquareController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        if (gameObject.transform.position.x > 8 || gameObject.transform.position.x < -8)
+        {
+            ChangeDirection();
+        }
+
+    }
+
+    private void ChangeDirection()
+    {
+        // Debug.Log("Byter riktning");
+
+        direction *= -1;
+    }
+
+
+    private void Move()
+    {
         Vector3 position = gameObject.transform.position;
 
-        position.x += 0.1f;
+        position.x += direction * speed;
 
         gameObject.transform.position = position;
-
     }
 }
